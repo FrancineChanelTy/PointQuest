8890202b44a39c562524d3426edd02953c38c4ce
jazzleebernabe09 <jazzleemartin49@gmail.com>
2026-09-11 14:01:51 +0800

    Information about me

---
+++ app/src/test/java/com/example/myapplication/quarter2/Gullaba_MiniPeta1.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.quarter2;
+
+import org.junit.Test;
+
+public class Gullaba_MiniPeta1{
+    @Test
+    public void printMyProfile() {
+
+        String myName = "Gullaba";
+        String petName = "  Baxter";
+        String favFood = "Bicol Express";
+        int myAge = 15;
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If  I could, I would eat " + favFood + " every single day!");
+
+    }
+}

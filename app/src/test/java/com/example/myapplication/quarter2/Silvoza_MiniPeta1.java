1d1b22bc713284de24e2d3243d8029036029715d
Silvoza <xsilvoza@gmail.com>
2026-09-14 13:45:04 +0800

    added java

---
+++ app/src/test/java/com/example/myapplication/quarter2/Silvoza_MiniPeta1.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.quarter2;
+
+import org.junit.Test;
+
+public class Silvoza_MiniPeta1{
+    @Test
+    public void printMyProfile() {
+
+        String myName = "Silvoza";
+        String petName = "Yuki";
+        String favFood = "Sisig tofu";
+        int myAge = 16;
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If  I could, I would eat " + favFood + " every single day!");
+
+    }
+}
